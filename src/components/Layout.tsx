44593fcea2486474497e68aa50e2e5786bea0a4a
D3Portillo <d3portillo@gmail.com>
2022-09-26 19:34:20 +0200

    chores: enhance Layout.tsx and add better wallet conection handling

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,22 +1,42 @@
-import type { PropsWithChildren } from "react"
+import { PropsWithChildren, useMemo } from "react"
 import Link from "next/link"
 import { useRouter } from "next/router"
+import { useAccount } from "wagmi"
 
+import {
+  ConnectButton,
+  useAccountModal,
+  useConnectModal,
+} from "@rainbow-me/rainbowkit"
 import { GrInbox } from "react-icons/gr"
 import { RiMailSendLine } from "react-icons/ri"
-import { ConnectButton } from "@rainbow-me/rainbowkit"
 
-import { classnames } from "@/lib/helpers"
+import emojiAvatarForAddress from "@/lib/emojiAvatarForAddress"
+import { classnames, noOp } from "@/lib/helpers"
 import RainbowInput from "./RainbowInput"
 import SendMessage from "./SendMessage"
 
-function NavButton({ children, href }: PropsWithChildren<{ href?: any }>) {
+function NavButton({
+  children,
+  href,
+  onClick,
+  style,
+  className,
+}: PropsWithChildren<{
+  href?: any
+  onClick?(): void
+  style?: {}
+  className?: string
+}>) {
   const router = useRouter()
   const isActive = router.route === href
   return (
     <Link href={href}>
       <a
+        onClick={onClick}
+        style={style}
         className={classnames(
+          className,
           isActive && "bg-zinc-50",
           "w-12 h-12 hover:bg-zinc-50 rounded-full flex items-center justify-center",
           "transition-transform ring-blue-500 duration-150 hover:ring-2 hover:scale-105 active:scale-95"
@@ -29,9 +49,31 @@ function NavButton({ children, href }: PropsWithChildren<{ href?: any }>) {
 }
 
 function Layout({ children }: PropsWithChildren<{}>) {
+  const { address } = useAccount()
+  const { openAccountModal } = useAccountModal()
+  const { openConnectModal = noOp } = useConnectModal()
+  const userMeta = useMemo(() => {
+    if (address) return emojiAvatarForAddress(address)
+    return {
+      color: "#FFE8E8",
+      emoji: "👻",
+    }
+  }, [address])
+
+  const onAccountPress = openAccountModal || openConnectModal
   return (
-    <div className="flex p-4 max-w-4xl mx-auto min-h-screen">
-      <div className="flex px-4 flex-col space-y-4 mb-4">
+    <div className="flex p-4 max-w-4xl flex-col md:flex-row mx-auto min-h-screen">
+      <div className="flex md:flex-col md:px-4 gap-4 mb-4">
+        <NavButton
+          onClick={onAccountPress}
+          style={{
+            background: userMeta.color,
+          }}
+          href="#"
+          className="md:hidden"
+        >
+          {userMeta.emoji}
+        </NavButton>
         <NavButton href="/">
           <GrInbox />
         </NavButton>
@@ -43,8 +85,14 @@ function Layout({ children }: PropsWithChildren<{}>) {
       </div>
       <div className="flex flex-col flex-grow space-y-4">
         <div className="flex items-center justify-between">
-          <RainbowInput placeholder="Search for an email" isPlain />
-          <ConnectButton showBalance={false} />
+          <RainbowInput
+            className="w-full md:max-w-xs"
+            placeholder="Search for an email"
+            isPlain
+          />
+          <div className="hidden md:block">
+            <ConnectButton showBalance={false} />
+          </div>
         </div>
         {children}
       </div>
