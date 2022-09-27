8df1a1a8a41afe5cf74dc027c245e12c364d6113
D3Portillo <d3portillo@gmail.com>
2022-09-27 02:32:40 +0200

    enh(MessageItem): add user profile visualization

--- src/components/MessageItem.tsx
+++ src/components/MessageItem.tsx
@@ -3,6 +3,7 @@ import { Fragment } from "react"
 import { RiShareForwardFill } from "react-icons/ri"
 import { GoReply } from "react-icons/go"
 
+import emojiAvatarForAddress from "@/lib/emojiAvatarForAddress"
 import { relativeFormat } from "@/lib/time"
 import useOnOffMachine from "@/lib/hooks/useOnOffMachine"
 import RainbowButton from "./RainbowButton"
@@ -13,18 +14,26 @@ function MessageItem({
   time,
   message,
   subject,
+  address,
 }: {
   time: number
   message: string
   subject: string
+  address: string
 }) {
   console.log({ time })
   const modal = useOnOffMachine()
+  const userMeta = emojiAvatarForAddress(address)
   return (
     <Fragment>
       <Drawer show={modal.isOn} onClose={modal.turnOff}>
         <div className="flex items-center space-x-4">
-          <div className="w-8 h-8 bg-black"></div>
+          <div
+            className="flex items-center justify-center w-8 h-8 rounded-full"
+            style={{ background: userMeta.color }}
+          >
+            {userMeta.emoji}
+          </div>
           <ExternalLink className="h-8">0x01a...df0</ExternalLink>
         </div>
         <div className="my-4 pt-4 border-t">
@@ -49,7 +58,12 @@ function MessageItem({
         onClick={modal.turnOn}
         className="cursor-pointer border-t max-w-xl flex items-center space-x-2 py-2"
       >
-        <div className="bg-black w-12 min-w-[3rem] h-12 rounded-full"></div>
+        <div
+          style={{ background: userMeta.color }}
+          className="flex items-center justify-center w-12 min-w-[3rem] h-12 rounded-full"
+        >
+          {userMeta.emoji}
+        </div>
         <div className="flex flex-col">
           <strong>{subject}</strong>
           <p>{message}</p>
