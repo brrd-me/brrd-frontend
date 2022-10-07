a62fa2e6fa643422998cdbbdd0b3452ba8f96a2a
D3Portillo <d3portillo@gmail.com>
2022-10-07 21:00:51 +0200

    feat(MessageItem): better UI and visuals

--- src/components/MessageItem.tsx
+++ src/components/MessageItem.tsx
@@ -1,78 +1,33 @@
-import { Fragment } from "react"
-
-import { RiShareForwardFill } from "react-icons/ri"
-import { GoReply } from "react-icons/go"
-
-import { beautifyAddress, getGoerliAddressURL } from "@/lib/helpers"
-import emojiAvatarForAddress from "@/lib/emojiAvatarForAddress"
+import { useEmojiAvatar } from "@/lib/emojiAvatarForAddress"
 import { relativeFormat } from "@/lib/time"
-import useOnOffMachine from "@/lib/hooks/useOnOffMachine"
-import RainbowButton from "./RainbowButton"
-import ExternalLink from "./ExternalLink"
-import Drawer from "./Drawer"
 
 function MessageItem({
   time,
   message,
   subject,
   address,
-}: {
-  time: number
-  message: string
-  subject: string
-  address: string
+  onClick,
+}: IEmail & {
+  onClick(): void
 }) {
-  const modal = useOnOffMachine()
-  const userMeta = emojiAvatarForAddress(address)
+  const avatar = useEmojiAvatar(address)
   return (
-    <Fragment>
-      <Drawer show={modal.isOn} onClose={modal.turnOff}>
-        <div className="flex items-center space-x-4">
-          <div
-            className="flex items-center justify-center w-8 h-8 rounded-full"
-            style={{ background: userMeta.color }}
-          >
-            {userMeta.emoji}
-          </div>
-          <ExternalLink href={getGoerliAddressURL(address)} className="h-8">
-            {beautifyAddress(address)}
-          </ExternalLink>
-        </div>
-        <div className="my-4 pt-4 border-t">
-          <strong>{subject}</strong>
-          <p>{message}</p>
-        </div>
-        <div className="flex-grow" />
-        <div className="flex items-center justify-end space-x-4 overflow-hidden">
-          <p className="text-xs">Sent {relativeFormat(time)}</p>
-          <div className="flex-grow" />
-          <RainbowButton className="flex items-center space-x-2" isPlain>
-            <GoReply />
-            <span>REPLY</span>
-          </RainbowButton>
-          <RainbowButton className="flex items-center space-x-2" isPlain>
-            <RiShareForwardFill />
-            <span>FORWARD</span>
-          </RainbowButton>
-        </div>
-      </Drawer>
+    <section
+      onClick={onClick}
+      className="cursor-pointer border-b last:border-none max-w-xl flex items-center space-x-2 py-2"
+    >
       <div
-        onClick={modal.turnOn}
-        className="cursor-pointer border-t max-w-xl flex items-center space-x-2 py-2"
+        style={{ background: avatar.color }}
+        className="flex items-center justify-center w-12 min-w-[3rem] h-12 rounded-full"
       >
-        <div
-          style={{ background: userMeta.color }}
-          className="flex items-center justify-center w-12 min-w-[3rem] h-12 rounded-full"
-        >
-          {userMeta.emoji}
-        </div>
-        <div className="flex flex-col">
-          <strong>{subject}</strong>
-          <p>{message}</p>
-          <p className="text-xs">{relativeFormat(time)}</p>
-        </div>
+        {avatar.emoji}
       </div>
-    </Fragment>
+      <div className="flex flex-col">
+        <strong>{subject}</strong>
+        <p>{message}</p>
+        <p className="text-xs">{relativeFormat(time)}</p>
+      </div>
+    </section>
   )
 }
 
--- src/components/SendMessage/ActionButton.tsx
+++ src/components/SendMessage/ActionButton.tsx
@@ -1,6 +1,5 @@
-import { IoMdCreate } from "react-icons/io"
-
 import { classnames } from "@/lib/helpers"
+import { RiMailAddFill } from "react-icons/ri"
 
 function ActionButton({ onClick }: { onClick(): void }) {
   return (
@@ -8,11 +7,11 @@ function ActionButton({ onClick }: { onClick(): void }) {
       onClick={onClick}
       className={classnames(
         "transition-transform ring-blue-500 duration-150 hover:ring-2 hover:scale-105 active:scale-95",
-        "w-12 h-12 bg-[#00FFC2] shadow-md rounded-full flex items-center justify-center",
-        "fixed md:relative bottom-0 right-0 m-4 md:m-0"
+        "w-12 h-12 bg-black shadow-md rounded-full flex items-center justify-center",
+        "fixed group md:relative bottom-0 right-0 m-4 md:m-0"
       )}
     >
-      <IoMdCreate className="text-black text-2xl" />
+      <RiMailAddFill className="text-white transform text-lg group-hover:-rotate-3" />
     </button>
   )
 }
