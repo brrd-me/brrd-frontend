54f7a64025cde392011e8ee6482f3a17d7beabcc
D3Portillo <d3portillo@gmail.com>
2022-10-07 19:55:10 +0200

    refactor(NavButton,NavItem): rename component adn enhance it's component API

---
+++ src/components/Layout/NavItem.tsx
@@ -0,0 +1,47 @@
+import type { PropsWithChildren } from "react"
+import Link from "next/link"
+import { useRouter } from "next/router"
+
+import { classnames } from "@/src/lib/helpers"
+
+function NavItem({
+  children,
+  href,
+  onClick,
+  style,
+  isButton,
+  className,
+}: PropsWithChildren<{
+  href?: any
+  onClick?(): void
+  style?: {}
+  isButton?: boolean
+  className?: string
+}>) {
+  const router = useRouter()
+  const isActive = router.route === href
+  const mergedClassName = classnames(
+    className,
+    isActive && "bg-zinc-50",
+    "w-12 h-12 hover:bg-zinc-50 rounded-full flex items-center justify-center",
+    "transition-transform ring-blue-500 duration-150 hover:ring-2 hover:scale-105 active:scale-95"
+  )
+
+  if (isButton) {
+    return (
+      <button onClick={onClick} style={style} className={mergedClassName}>
+        {children}
+      </button>
+    )
+  }
+
+  return (
+    <Link href={href}>
+      <a style={style} className={mergedClassName}>
+        {children}
+      </a>
+    </Link>
+  )
+}
+
+export default NavItem
