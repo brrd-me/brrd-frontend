732bb879973a12ac8b789472496e82d683dde0ea
D3Portillo <d3portillo@gmail.com>
2022-10-07 21:01:54 +0200

    refactor: unify layout updates for Message items and content responsiveness

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,47 @@
 import type { NextPage } from "next"
 
+import { classnames } from "@/lib/helpers"
+
 import Layout from "@/components/Layout"
 import MessageItem from "@/components/MessageItem"
 import useReadEmails from "@/contracts/useReadEmails"
+import MessagePreview from "@/components/MessagePreview"
 
 const HomePage: NextPage = () => {
-  const { emails, isEmpty } = useReadEmails("emailsReceived")
+  const { emails, isEmpty, previewEmail, showPreview } =
+    useReadEmails("emailsReceived")
+
   return (
     <Layout>
       <strong>Inbox</strong>
-      <div className="flex flex-col">
-        {isEmpty && <p>Empty :{"("}</p>}
-        {emails.map((email) => {
-          return (
-            <MessageItem
-              address={email.sender}
-              time={email.time.toNumber()}
-              message={email.message}
-              subject={email.subject}
-              key={`email-item-${email.time}`}
-            />
-          )
-        })}
+      <div className="flex border-t flex-grow">
+        <div
+          className={classnames(
+            showPreview && "max-w-sm",
+            "flex flex-col w-full"
+          )}
+        >
+          {isEmpty && <p>Empty :{"("}</p>}
+          {emails.map((email) => {
+            return (
+              <MessageItem
+                onClick={email.setAsPreviewEmail}
+                address={email.sender}
+                time={email.time.toNumber()}
+                message={email.message}
+                subject={email.subject}
+                key={`email-item-${email.time}`}
+              />
+            )
+          })}
+        </div>
+        <MessagePreview
+          show={showPreview}
+          time={previewEmail.time}
+          subject={previewEmail.subject}
+          message={previewEmail.message}
+          address={previewEmail.sender}
+        />
       </div>
     </Layout>
   )
--- src/pages/sent.tsx
+++ src/pages/sent.tsx
@@ -1,27 +1,46 @@
 import type { NextPage } from "next"
 
-import MessageItem from "@/components/MessageItem"
+import { classnames } from "@/lib/helpers"
+
 import Layout from "@/components/Layout"
+import MessageItem from "@/components/MessageItem"
 import useReadEmails from "@/contracts/useReadEmails"
+import MessagePreview from "@/components/MessagePreview"
 
 const SentPage: NextPage = () => {
-  const { emails, isEmpty } = useReadEmails("emailsSent")
+  const { emails, isEmpty, previewEmail, showPreview } =
+    useReadEmails("emailsSent")
   return (
     <Layout>
       <strong>Sent</strong>
-      <div className="flex flex-col">
-        {isEmpty && <p>Empty :{"("}</p>}
-        {emails.map((email) => {
-          return (
-            <MessageItem
-              address={email.receiver}
-              time={email.time.toNumber()}
-              message={email.message}
-              subject={email.subject}
-              key={`email-item-${email.time}`}
-            />
-          )
-        })}
+      <div className="flex border-t flex-grow">
+        <div
+          className={classnames(
+            showPreview && "max-w-sm",
+            "flex flex-col w-full"
+          )}
+        >
+          {isEmpty && <p>Empty :{"("}</p>}
+          {emails.map((email) => {
+            return (
+              <MessageItem
+                onClick={email.setAsPreviewEmail}
+                address={email.receiver}
+                time={email.time.toNumber()}
+                message={email.message}
+                subject={email.subject}
+                key={`email-item-${email.time}`}
+              />
+            )
+          })}
+        </div>
+        <MessagePreview
+          show={showPreview}
+          time={previewEmail.time}
+          subject={previewEmail.subject}
+          message={previewEmail.message}
+          address={previewEmail.receiver}
+        />
       </div>
     </Layout>
   )
